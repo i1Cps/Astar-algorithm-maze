32c0bbca3228632e9d5b94df02ed0278531c1bed
i1Cps <nohacks2701@gmail.com>
2023-09-09 22:42:41 +0100

    Updated logic and formatted nicely

--- src/main.ts
+++ src/main.ts
@@ -1,145 +1,138 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { AxesHelper } from "three";
 import { Maze } from "./maze.js";
 import { AStarMazeSolver } from "./aStarMazeSolver.js";
-import KeyboardInputs from "./keyboardInputs.js";
 
 // MAIN
 
 // GLOBAL CONSTANTS
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 const boardSize = 200;
 const rows = 10;
 const columns = 10;
-const spherical = new THREE.Spherical();
 
 // STANDARD GLOBAL CONSTANTS AND VARIABLES
 var scene: THREE.Scene;
 var camera: THREE.PerspectiveCamera;
 var renderer: THREE.WebGLRenderer;
 var controls: OrbitControls;
-var keyboardInput: KeyboardInputs;
-KeyboardInputs.initialize();
 
 // CUSTOM GLOBAL CONSTANTS AND VARIABLES
-var topCamera;
 let solver: AStarMazeSolver;
 let maze: Maze;
-let ticktock: boolean;
 
 init();
 animate();
 
 // FUNCTIONS
 function init() {
-    scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-    // CAMERA
-    var VIEW_ANGLE = 75,
-        ASPECT = sizes.width / sizes.height,
-        NEAR = 0.1,
-        FAR = 20000;
-    // camera 1
-    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+  // CAMERA
+  var VIEW_ANGLE = 75,
+    ASPECT = sizes.width / sizes.height,
+    NEAR = 0.1,
+    FAR = 20000;
+  // camera 1
+  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 
-    camera.position.set(0, 10, 250);
+  camera.position.set(0, 10, 250);
 
-    // RENDERER
-    const canvasElement = document.querySelector("#bg") as HTMLCanvasElement;
-    if (canvasElement) {
-        renderer = new THREE.WebGLRenderer({
-            canvas: canvasElement,
-        });
-    }
-
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    renderer.setSize(sizes.width, sizes.height);
-
-    // EVENTS
-    window.addEventListener("resize", () => {
-        //update sizes
-        sizes.width = window.innerWidth;
-        sizes.height = window.innerHeight;
-
-        //update cameras
-        camera.aspect = sizes.width / sizes.height;
-        camera.updateProjectionMatrix();
-
-        //update renderer
-        renderer.setSize(sizes.width, sizes.height);
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  // RENDERER
+  const canvasElement = document.querySelector("#bg") as HTMLCanvasElement;
+  if (canvasElement) {
+    renderer = new THREE.WebGLRenderer({
+      canvas: canvasElement,
     });
+  }
 
-    // CONTROLS
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.maxAzimuthAngle = 0.5;
-    controls.minAzimuthAngle = -0.5;
-    controls.maxPolarAngle = 2.05;
-    controls.minPolarAngle = 1.1;
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setSize(sizes.width, sizes.height);
 
-    // LIGHT
-    const pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.set(100, 50, 100);
-    const ambientLight = new THREE.AmbientLight(0xffffff);
-    scene.add(pointLight);
-    //scene.add(ambientLight, pointLight)
+  // EVENTS
+  window.addEventListener("resize", () => {
+    //update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
-    // HELPERS
-    const lightHelper = new THREE.PointLightHelper(pointLight);
-    const gridHelper = new THREE.GridHelper(200, 50);
-    const axisHelper = new THREE.AxesHelper(100);
-    //scene.add(lightHelper,  axisHelper);
+    //update cameras
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
-    //Space background
-    const spaceTexture = new THREE.TextureLoader().load("space.jpg");
-    scene.background = spaceTexture;
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
 
-    maze = new Maze(200, 10, 10, scene);
-    maze.setup();
+  // CONTROLS
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.maxAzimuthAngle = 0.87;
+  controls.minAzimuthAngle = -0.87;
+  controls.maxPolarAngle = 2.25;
+  controls.minPolarAngle = 0.9;
+  controls.enableZoom = false;
+
+  // LIGHT
+  // LIGHT
+  const pointLight = new THREE.PointLight(0xffffff);
+  pointLight.position.set(100, 50, 100);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+  scene.add(pointLight, ambientLight);
+
+  // HELPERS
+  const lightHelper = new THREE.PointLightHelper(pointLight);
+  const gridHelper = new THREE.GridHelper(200, 50);
+  const axisHelper = new THREE.AxesHelper(100);
+  //scene.add(lightHelper,  axisHelper);
+
+  //Space background
+  const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+  scene.background = spaceTexture;
+
+  maze = new Maze(200, 10, 10, scene);
+  maze.setup();
 }
 
 //Animate
 function animate() {
-    // Generate the maze
-    if (maze.getMazeComplete() === false) {
-        maze.createMaze();
-    } else {
-        // If maze is not currently being solved, initialize the solving setup function
-        if (maze.getSolving() == false) {
-            maze.setSolving(true);
-            let mazeGrid = maze.getMazeGrid();
-            let start = mazeGrid[0][0];
-            let finish = mazeGrid[rows - 1][columns - 1];
-            let mazeSizes = [boardSize, rows, columns];
-            solver = new AStarMazeSolver(start, finish, mazeGrid, mazeSizes);
-            solver.setup();
-        }
-
-        // recusively call
-        else {
-            if (solver.getSolved() == false) {
-                solver.solveStep();
-            } else if (solver.getBackTracking() == true) {
-                solver.solveStep();
-            }
-        }
+  // Generate the maze
+  if (maze.isMazeComplete() === false) {
+    maze.createMaze();
+  } else {
+    // If maze is not currently being solved, initialize the solving setup function
+    if (maze.isSolving() == false) {
+      maze.setSolving(true);
+      let mazeGrid = maze.getMazeGrid();
+      let start = mazeGrid[0][0];
+      let finish = mazeGrid[rows - 1][columns - 1];
+      let mazeSizes = [boardSize, rows, columns];
+      solver = new AStarMazeSolver(start, finish, mazeGrid, mazeSizes);
+      solver.setup();
     }
 
-    window.requestAnimationFrame(animate);
-    render();
-    update();
+    // recusively call
+    else {
+      if (solver.getSolved() == false) {
+        solver.solveStep();
+      } else if (solver.getBackTracking() == true) {
+        solver.solveStep();
+      }
+    }
+  }
+
+  window.requestAnimationFrame(animate);
+  render();
+  update();
 }
 
 function update() {
-    KeyboardInputs.update();
-    controls.update();
+  controls.update();
 }
 
 function render() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
--- src/maze.ts
+++ src/maze.ts
@@ -4,154 +4,192 @@ import { Colours } from "./interfaces/colours";
 
 // This class contains the logic for the maze itself
 export class Maze {
-    private size: number;
-    private rows: number;
-    private columns: number;
-    private grid: MazeCell[][];
-    private stack: MazeCell[];
-    private mazeComplete: boolean;
-    private current: MazeCell | null;
-    private scene: THREE.Scene;
-    private solving: boolean;
-    private width: number;
-    private height: number;
+  private size: number;
+  private rows: number;
+  private columns: number;
+  private grid: MazeCell[][];
+  private stack: MazeCell[];
+  private mazeComplete: boolean;
+  private current: MazeCell | null;
+  private scene: THREE.Scene;
+  private solving: boolean;
+  private width: number;
+  private height: number;
+  private plane: THREE.Mesh | null;
+  private mazeExist: boolean;
 
-    constructor(
-        size: number,
-        rows: number,
-        columns: number,
-        scene: THREE.Scene
-    ) {
-        this.size = size;
-        this.rows = rows;
-        this.columns = columns;
-        this.grid = [];
-        this.stack = [];
-        this.mazeComplete = false;
-        this.current = null;
-        this.scene = scene;
-        this.solving = false;
-        this.width = size;
-        this.height = size;
+  constructor(size: number, rows: number, columns: number, scene: THREE.Scene) {
+    this.size = size;
+    this.rows = rows;
+    this.columns = columns;
+    this.grid = [];
+    this.stack = [];
+    this.mazeComplete = false;
+    this.current = null;
+    this.scene = scene;
+    this.solving = false;
+    this.width = size;
+    this.height = size;
+    this.plane = null;
+    this.mazeExist = true;
+  }
+
+  // This creates a full grid based on size, rows and columns params
+  setup() {
+    // Creates cells and pushes them into grid using nested loop
+    for (let r = 0; r < this.rows; r++) {
+      let row = [];
+      for (let c = 0; c < this.columns; c++) {
+        let cell = new MazeCell(r, c, this.grid, this.size, this.scene);
+        row.push(cell);
+      }
+      this.grid.push(row);
+    }
+    // First cell in grid should be our starting point
+    this.current = this.grid[0][0];
+    this.current.setConnected(true);
+
+    // Draws the grid with four walls around each cell
+    for (let r = 0; r < this.rows; r++) {
+      for (let c = 0; c < this.columns; c++) {
+        let grid = this.grid;
+        grid[r][c].updateWalls(this.size, this.rows, this.columns);
+      }
     }
 
-    // This creates a full grid based on size, rows and columns params
-    setup() {
-        // Creates cells and pushes them into grid using nested loop
-        for (let r = 0; r < this.rows; r++) {
-            let row = [];
-            for (let c = 0; c < this.columns; c++) {
-                let cell = new MazeCell(r, c, this.grid, this.size, this.scene);
-                row.push(cell);
-            }
-            this.grid.push(row);
+    // CREATE MAZE BACKGROUND
+    this.plane = new THREE.Mesh(
+      new THREE.PlaneGeometry(this.size, this.size, 8, 8),
+      new THREE.MeshBasicMaterial({
+        color: Colours.whiteColour,
+        side: THREE.DoubleSide,
+      })
+    );
+    this.scene.add(this.plane);
+  }
+
+  // Create Maze
+  createMaze() {
+    if (!this.scene) {
+      return;
+    }
+    if (this.current) {
+      if (this.mazeComplete === false) {
+        // Randomly choose neighbour as new cell
+        let next = this.current.checkNeighbours();
+
+        // remove active square colour highlight from "current" cell and add it to "next" cell
+        this.current.removeCellColour(this.current, Colours.greenColour);
+
+        // If neighbour cell was found
+        if (next) {
+          next.setConnected(true);
+          // Put active coloured square in cell
+          next.fillCellColour(
+            next.getColumnNumber(),
+            next.getRowNumber(),
+            this.size,
+            this.rows,
+            this.columns,
+            Colours.greenColour,
+            false
+          );
+          this.stack.push(this.current);
+
+          // Remove wall between given current cell and its neighbour cell 'next'
+          this.current.removeWall(this.current, next);
+          this.current = next;
+        } else if (this.stack.length > 0) {
+          // This part is basically backtracking if its hit a dead end
+          // back tracks to the last cell in the stack that has a non connected neighbour
+          let cell = this.stack.pop();
+          if (cell) {
+            this.current = cell;
+            // Put active colour square in cell
+            cell.fillCellColour(
+              cell.getColumnNumber(),
+              cell.getRowNumber(),
+              this.size,
+              this.rows,
+              this.columns,
+              Colours.greenColour,
+              false
+            );
+          }
         }
-        // First cell in grid should be our starting point
-        this.current = this.grid[0][0];
-        this.current.setConnected(true);
 
-        // Draws the grid with four walls around each cell
-        for (let r = 0; r < this.rows; r++) {
-            for (let c = 0; c < this.columns; c++) {
-                let grid = this.grid;
-                grid[r][c].updateWalls(this.size, this.rows, this.columns);
-            }
+        // If no neighbour cells are found and stack is empty the maze has been generated
+        if (this.stack.length === 0) {
+          console.log("Maze has been generated");
+          // Remove active colour square since maze has been generated
+          this.current.removeCellColour(this.current, Colours.greenColour);
+          this.mazeComplete = true;
         }
+        return;
+      } else {
+        console.log("Maze already generated");
+      }
+    } else {
+      console.log("Setup function was not executed properly");
+    }
+  }
 
-        // CREATE MAZE BACKGROUND
-        var plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(this.size, this.size, 8, 8),
-            new THREE.MeshBasicMaterial({
-                color: Colours.whiteColour,
-                side: THREE.DoubleSide,
-            })
-        );
-        this.scene.add(plane);
+  // General function to remove objects from scene
+  removeObject3D(object3D: THREE.Mesh | null): boolean {
+    if (!(object3D instanceof THREE.Mesh)) {
+      return false;
     }
 
-    // Create Maze
-    createMaze() {
-        if (this.current) {
-            if (this.mazeComplete == false) {
-                // Randomly choose neighbour as new cell
-                let next = this.current.checkNeighbours();
+    // For better memory management and performance
+    if (object3D.geometry) {
+      object3D.geometry.dispose();
+    }
 
-                // remove green highlight from "current" cell and add it to "next" cell
-                this.current.removeCellColour(
-                    this.current,
-                    Colours.greenColour
-                );
+    if (object3D.material instanceof Array) {
+      // For better memory management and performance
+      object3D.material.forEach((material) => material.dispose());
+    } else {
+      // For better memory management and performance
+      object3D.material.dispose();
+    }
+    // Default parent will be the scene so this good
+    object3D.removeFromParent();
+    return true;
+  }
 
-                // If neighbour cell was found
-                if (next) {
-                    next.setConnected(true);
-                    // Put green square in cell
-                    next.fillCellColour(
-                        next.getColumnNumber(),
-                        next.getRowNumber(),
-                        this.size,
-                        this.rows,
-                        this.columns,
-                        Colours.greenColour,
-                        false
-                    );
-                    this.stack.push(this.current);
-
-                    // Remove wall between given current cell and its neighbour cell 'next'
-                    this.current.removeWall(this.current, next);
-                    this.current = next;
-                } else if (this.stack.length > 0) {
-                    // This part is basically backtracking if its hit a dead end
-                    // back tracks to the last cell in the stack that has a non connected neighbour
-                    let cell = this.stack.pop();
-                    if (cell) {
-                        this.current = cell;
-                        // Put green square in cell
-                        cell.fillCellColour(
-                            cell.getColumnNumber(),
-                            cell.getRowNumber(),
-                            this.size,
-                            this.rows,
-                            this.columns,
-                            Colours.greenColour,
-                            false
-                        );
-                    }
-                }
-
-                // If no neighbour cells are found and stack is empty the maze has been generated
-                if (this.stack.length == 0) {
-                    console.log("Maze has been generated");
-                    // Remove green square since maze has been generated
-                    this.current.removeCellColour(
-                        this.current,
-                        Colours.greenColour
-                    );
-                    this.mazeComplete = true;
-                }
-                return;
-            } else {
-                console.log("Maze already generated");
-            }
-        } else {
-            console.log("Setup function was not executed properly");
+  // This function nicely cleans up all three js objects from the maze
+  destroyMaze() {
+    // First remove all 3D objects, then set properties to null
+    if (this.mazeExist) {
+      // All cells can be found in maze.grid
+      for (const cellRow of this.grid) {
+        for (const cell of cellRow) {
+          // Handle each of the cells objects
+          cell.removeAllCellObjects();
         }
+      }
+      this.removeObject3D(this.plane);
+      this.mazeExist = false;
     }
+  }
 
-    // Getters and Setters
-    getSolving(): boolean {
-        return this.solving;
-    }
+  // Getters and Setters
+  isSolving(): boolean {
+    return this.solving;
+  }
 
-    setSolving(value: boolean) {
-        this.solving = value;
-    }
+  getMazeExist(): boolean {
+    return this.mazeExist;
+  }
+  setSolving(value: boolean) {
+    this.solving = value;
+  }
 
-    getMazeComplete(): boolean {
-        return this.mazeComplete;
-    }
+  isMazeComplete(): boolean {
+    return this.mazeComplete;
+  }
 
-    getMazeGrid(): MazeCell[][] {
-        return this.grid;
-    }
+  getMazeGrid(): MazeCell[][] {
+    return this.grid;
+  }
 }
